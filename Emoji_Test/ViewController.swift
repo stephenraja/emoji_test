3b8623b1d0a2a1f00aee064c796b30211384d5e2
Stephen Raja Roby <coc.mobile.app@gmail.com>
2017-05-15 21:37:38 -0700

    Initial Commit

---
+++ Emoji_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji_Test
+//
+//  Created by Stephen Raja Roby on 5/15/17.
+//  Copyright © 2017 Cloud of Carmel Mobile App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
